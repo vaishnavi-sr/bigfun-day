a5e3ad498b2ebdbe6aafe70d75a70be1cd5ed8b5
vaishnavi <vaishu.srini15@gmail.com>
2023-02-23 13:53:24 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/bigfunday/BigfunDayApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bigfunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfunDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigfunDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/bigfunday/BigfunDayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bigfunday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigfunDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
