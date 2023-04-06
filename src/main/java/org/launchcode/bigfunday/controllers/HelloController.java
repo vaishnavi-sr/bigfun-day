221d50e232336cb0123e57186c6f41d19590b130
vaishnavi <vaishu.srini15@gmail.com>
2023-04-06 12:35:45 -0500

    QueryParam for name

--- src/main/java/org/launchcode/bigfunday/controllers/HelloController.java
+++ src/main/java/org/launchcode/bigfunday/controllers/HelloController.java
@@ -3,13 +3,27 @@ package org.launchcode.bigfunday.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping
+//    @GetMapping
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, have a nice day!";
+//    }
+
+    @GetMapping("goodbye")
     @ResponseBody
-    public String hello(){
-        return "Hello, have a nice day!";
+    public String goodbye() {
+        return "Goodbye, Have a really big fun day!";
+    }
+
+    @GetMapping("hello") //lives @ /hello?name=LaunchCode
+    @ResponseBody
+    public String helloQuery(@RequestParam String name){
+        return "Hello, Are you having a big fun day "+ name + "?";
     }
 }
