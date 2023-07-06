d6b79e5e3db7efaa129483906cf2479dafde5829
vaishnavi <vaishu.srini15@gmail.com>
2023-07-06 14:27:25 -0500

    Added form

--- src/main/java/org/launchcode/bigfunday/controllers/HelloController.java
+++ src/main/java/org/launchcode/bigfunday/controllers/HelloController.java
@@ -2,10 +2,7 @@ package org.launchcode.bigfunday.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -14,6 +11,13 @@ public class HelloController {
 //    public String hello() {
 //        return "Hello, have a nice day!";
 //    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!. Hello, Are you having a big fun day "+ name + "?. Goodbye, Have a really big fun day!";
+    }
 
     @GetMapping("goodbye")
     @ResponseBody
@@ -25,5 +29,16 @@ public class HelloController {
     @ResponseBody
     public String helloQuery(@RequestParam String name){
         return "Hello, Are you having a big fun day "+ name + "?";
+    }
+
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
     }
 }
